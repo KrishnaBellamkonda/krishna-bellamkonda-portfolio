c34bb4d6e5f9147ed452f0316a76df48692ae1a2
Bellamkonda Sri Krishna Chaitanya <87589090+KrishnaBellamkonda@users.noreply.github.com>
2021-10-25 21:03:15 +0100

    Update firebase.js
    deleted passwords and api ids

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase'
 // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
-    apiKey: "AIzaSyA-Lz6Z-MsBfcQSvaqa-ItAEfrklXOxrK4",
-    authDomain: "portfoliocontactform-9d41c.firebaseapp.com",
-    databaseURL: "https://portfoliocontactform-9d41c-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "portfoliocontactform-9d41c",
-    storageBucket: "portfoliocontactform-9d41c.appspot.com",
-    messagingSenderId: "344492710937",
-    appId: "1:344492710937:web:417c218136495d83531e88",
-    measurementId: "G-4B0G22YQ2R"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   // Initialize Firebase
 
